d44292809e0ec1e7c344006ae7733d2bf620778b
MartaCEI <guecolmar@gmail.com>
2024-07-16 22:14:56 +0200

    Updates

--- js/excursiones.js
+++ js/excursiones.js
@@ -1,28 +1,25 @@
 `use strict`
 
 // HEADER 
-const header = document.querySelector('.Header')
-const headerBtn = document.querySelector('.Header-btn')
-const headerNav = document.querySelector('.Header-nav')
+const header = document.querySelector('.Header-excursiones')
+const headerBtn = header.querySelector('.Header-btn')
+const headerNav = header.querySelector('.Header-nav')
 
 headerBtn.addEventListener('click', () => {
     headerNav.classList.toggle('isActive')
 })
 
 // Efecto paralax
+
 const excursionesBackGround = document.querySelector('.Excursiones-background')
 const excursionesH1 = document.querySelector('.Excursiones-h1')
-console.log(excursionesH1)
 
-console.log(document.documentElement.scrollTop)
-console.log(window.scrollY)
-
-function parallax () {
+const parallax = () => {
     let scrollPosition = window.scrollY
-    excursionesBackGround.style.marginTop = (scrollPosition / 1.3) + 'px';
-    excursionesH1.style.bottom = (scrollPosition / 6) + 'px';
-    excursionesH1.style.opacity = 1 - (scrollPosition / 1500);
-}
+    excursionesBackGround.style.backgroundPosition = 'center ' + (-scrollPosition / 4) + 'px'
+    excursionesH1.style.marginTop = (scrollPosition / 3) + 'px'
+    excursionesH1.style.opacity = 1 - (scrollPosition / 300)
+};
 
 window.addEventListener('scroll', () => {
     parallax()
